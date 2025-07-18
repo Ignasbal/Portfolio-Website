49f9d464c6d7c3c840aab42b960161087315abce
Ignasbal <ignaswind@gmail.com>
2025-07-18 19:57:16 +0300

    Added mobile side menu

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,9 +1,26 @@
+"";
+
 import { assets } from "@/assets/assets";
 import styles from "./styles.module.css";
 import Image from "next/image";
 import Link from "next/link";
+import { useRef } from "react";
 
 const Header = () => {
+  const sideMenuRef = useRef();
+
+  const openSideMenu = () => {
+    if (sideMenuRef.current) {
+      sideMenuRef.current.style.transform = "translateX(-16rem)";
+    }
+  };
+
+  const closeSideMenu = () => {
+    if (sideMenuRef.current) {
+      sideMenuRef.current.style.transform = "translateX(16rem)";
+    }
+  };
+
   return (
     <div>
       <nav className={styles.header}>
@@ -25,15 +42,66 @@ const Header = () => {
             </li>
           </ul>
         </div>
-        <Link href="#" className={styles.contactLink}>
-          {" "}
-          <h1>Contact</h1>{" "}
-          <Image
-            src={assets.arrow_icon}
-            className={styles.arrowIcon}
-            alt="Arrow icon"
-          />
-        </Link>
+        <div className={styles.moon_icon_and_contactLink}>
+          <button>
+            {" "}
+            <Image
+              src={assets.moon_icon}
+              alt="Moon Icon"
+              className={styles.moon_icon}
+            />
+          </button>
+          <Link href="#" className={styles.contactLink}>
+            {" "}
+            <h1>Contact</h1>{" "}
+            <Image
+              src={assets.arrow_icon}
+              className={styles.arrowIcon}
+              alt="Arrow icon"
+            />
+          </Link>
+
+          <button onClick={openSideMenu} className={styles.menuButton}>
+            <Image
+              src={assets.menu_black}
+              alt="Menu Icon"
+              className={styles.menu_icon}
+            />
+          </button>
+        </div>
+
+        {/*------Mobile menu------ */}
+
+        <ul ref={sideMenuRef} className={styles.mobileMenu}>
+          <div className={styles.closeIconWrapper} onClick={closeSideMenu}>
+            <Image
+              src={assets.close_black}
+              alt="Close icon"
+              className={styles.closeIcon}
+            />
+          </div>
+
+          <li>
+            <Link href="#top" onClick={closeSideMenu}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link href="#about" onClick={closeSideMenu}>
+              About me
+            </Link>
+          </li>
+          <li>
+            <Link href="#services" onClick={closeSideMenu}>
+              Services
+            </Link>
+          </li>
+          <li>
+            <Link href="#work" onClick={closeSideMenu}>
+              My Work
+            </Link>
+          </li>
+        </ul>
       </nav>
     </div>
   );
